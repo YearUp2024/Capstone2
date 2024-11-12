87caf88c8138c8221dfab67aab4ff0182a84208b
iftekar2 <iftekar.hossain123@gmail.com>
2024-11-11 19:17:43 -0500

    OrderScreen Class is able to handle invalid inputs

--- src/main/java/com/pluralsight/HomeScreen/OrderScreen.java
+++ src/main/java/com/pluralsight/HomeScreen/OrderScreen.java
@@ -10,57 +10,71 @@ public class OrderScreen {
     ToppingsOptions toppingsOptions = new ToppingsOptions();
 
     public void userOrder() {
-        List<String> menuOptions;
-
+        //List<String> menuOptions;
         String order = "";
+
         do {
-            System.out.println("Please select from options: ");
-            menuOptions = toppingsOptions.mainMenue();
+            int userChoice = 0;
+            boolean validInput = false;
+            while(!validInput){
+                System.out.println("Please choose from the options: ");
+                toppingsOptions.mainMenue();
 
-            int userChoice = Console.PromptForInt("Enter your choice: ");
-            do{
-                System.out.println("Please select from options: ");
-                menuOptions = toppingsOptions.mainMenue();
-
-                if(userChoice == 1){
-                    order = "sandwich";
-                }else if(userChoice == 2){
-                    order = "drink";
-                }else if(userChoice == 3){
-                    order = "chips";
-                }else if(userChoice == 4){
-                    order = "checkout";
-                }else if(userChoice == 5){
-                    order = "cancel order";
+                try{
+                    userChoice = Console.PromptForInt("Please enter your Choice: ");
+                    if(userChoice >= 1 && userChoice <= 5){
+                        validInput = true;
+                    }
+                }catch(Exception e){
+                    System.out.println("\nYour input is invalid. Please enter a number between 1 and 5.");
                 }
-            }while(userChoice != 1 || userChoice != 2 || userChoice != 3 || userChoice != 4 || userChoice != 5);
+            }
+
+            switch(userChoice){
+                case 1:
+                    order = "sandwich";
+                    break;
+                case 2:
+                    order = "drink";
+                    break;
+                case 3:
+                    order = "chips";
+                    break;
+                case 4:
+                    order = "checkout";
+                    break;
+                case 5:
+                    order = "cancel order";
+                    break;
+                default:
+                    System.out.println("Your choice is incorrect.");
+                    continue;
+            }
             System.out.println();
 
-            if (menuOptions.contains(order)){
-                switch (order) {
-                    case "sandwich":
-                        sandwichInterface.orderSandwich();
-                        System.out.println();
-                        break;
-                    case "drink":
-                        System.out.println("Add Drink");
-                        System.out.println();
-                        break;
-                    case "chips":
-                        System.out.println("Add Chips");
-                        System.out.println();
-                        break;
-                    case "checkout":
-                        System.out.println("Checkout");
-                        System.out.println();
-                        break;
-                    case "cancel order":
-                        System.out.println("Your order is canceled.");
-                        break;
-                    default:
-                        System.out.println("Your choice is incorrect.");
-                        break;
-                }
+            switch (order) {
+                case "sandwich":
+                    sandwichInterface.orderSandwich();
+                    System.out.println();
+                    break;
+                case "drink":
+                    System.out.println("Add Drink");
+                    System.out.println();
+                    break;
+                case "chips":
+                    System.out.println("Add Chips");
+                    System.out.println();
+                    break;
+                case "checkout":
+                    System.out.println("Checkout");
+                    System.out.println();
+                    break;
+                case "cancel order":
+                    System.out.println("Your order is canceled.");
+                    break;
+                default:
+                    System.out.println("Your choice is incorrect.");
+                    break;
             }
         } while (!order.equalsIgnoreCase("cancel order"));
     }
